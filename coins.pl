18c8308292a3069aaade994bb691d5244f8bc3e5
deanturpin <kali@brightcoin.uk>
2018-10-14 15:09:59 +0100

    Get results

--- coins.pl
+++ coins.pl
@@ -10,19 +10,48 @@ try:
     r = requests.get(url)
     exchanges = r.json()
 
+    results = []
+    sym1 = "BTC"
+    sym2 = "GBP"
+
     # Look for exchange that offer the pairs we're interested in
     for name in exchanges:
       for from_symbol in exchanges[name]:
         found1 = False
         found2 = False
         for to_symbol in exchanges[name][from_symbol]:
-          if to_symbol == "BTC": found1 = True
-          if to_symbol == "GBP": found2 = True
+          if to_symbol == sym1: found1 = True
+          if to_symbol == sym2: found2 = True
 
           # If we've found them all then jump to next pair
           if found1 and found2:
-            print (name + "\t" + from_symbol)
+            results.append([name, from_symbol, sym1])
+            results.append([name, from_symbol, sym2])
             break
+
+    # Dump the results
+    # print("Results " + str(len(results)))
+    from_symbols = set()
+    to_symbols = sym1 + "," + sym2
+    for trade in results:
+    # print (trade[0] + "\t" + trade[1] + "\t" + trade[2])
+      from_symbols.add(trade[1])
+
+    url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms="
+    for sym in from_symbols:
+            url += sym + ","
+            
+    url += "&tsyms=" + to_symbols
+    # print(url)
+    r = requests.get(url)
+
+    matrix = r.json();
+
+    for coin in matrix:
+      print(coin, end=" ")
+      for price in matrix[coin]:
+        print(matrix[coin][price], end=" ")
+      print("")
 
 except Exception as e:
     print("exception ", e)
