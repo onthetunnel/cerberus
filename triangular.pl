b5b00eb44696e31f91782e0e069692ea5a9f787f
deanturpin <kali@brightcoin.uk>
2018-10-15 10:00:59 +0100

    Complete trianglar arbitrage

--- triangular.pl
+++ triangular.pl
@@ -17,8 +17,9 @@ try:
     r = requests.get(url)
     all_coins = r.json()
 
-    entry_currency = "HRK"
-    exit_currency = "GBP"
+    entry_currency = "GBP"
+    exit_currency = "HRK"
+    entry_value = 1
 
     # Construct a list of URLs for the currency pair we're interested in
     currency_pairs = []
@@ -31,49 +32,32 @@ try:
 
     entry_points = []
     exit_points = []
+    entry_coins = set()
     for pair in currency_pairs:
       if pair[2] == entry_currency:
         entry_points.append(pair)
+        entry_coins.add(pair[1])
       if pair[2] == exit_currency:
         exit_points.append(pair)
 
-    print(len(entry_points), "entry points")
-    for trade in entry_points:
-      url = construct_url(trade[0], trade[1], trade[2])
-      r = requests.get(url)
-      price = r.json()
-      print(trade, price)
+    print(entry_coins, "entry coins")
 
     print(len(exit_points), "exit points")
     for trade in exit_points:
-      url = construct_url(trade[0], trade[1], trade[2])
-      r = requests.get(url)
-      price = r.json()
-      print(trade, price)
-
-    # Get latest rates
-    # prices = []
-    # for t in trades:
-    #   exchange = t[0]
-    #   url = construct_url(exchange, t[1], t[2])
-    #   r = requests.get(url)
-    #   price = r.json()
-    #   prices.append([exchange, float(price[sym2])])
-    #       
-    # # Calculate exchange ratios between exchanges
-    # i = 0
-    # ri = len(prices) - 1
-    # rates = []
-    # while i < len(prices):
-    #   rates.append([prices[i][1] / prices[ri][1], prices[i][0]
-    #                   + " > " + prices[ri][0]])
-    #   i = i + 1
-    #   ri = ri - 1
-
-    # # Sort and report
-    # rates.sort()
-    # for r in rates:
-    #   print(r[0], "\t", r[1])
+      if trade[1] in entry_coins:
+        url = construct_url(trade[0], trade[1], trade[2])
+        r = requests.get(url)
+        price = r.json()
+        exchange_value = entry_value / float(price[trade[2]])
+        print("EXIT\t", trade, price[trade[2]], "\t", exchange_value)
+        for entry in entry_points:
+          if entry[1] == trade[1]:
+          # print("ENTRY\t", entry)
+            url = construct_url(entry[0], entry[1], entry[2])
+            r = requests.get(url)
+            price = r.json()
+            exit_value = exchange_value * float(price[entry[2]])
+            print("\t", entry, price[entry[2]], "\t", exit_value)
 
 except Exception as e:
     print("exception ", e)
