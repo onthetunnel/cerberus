fa1f30ec264900e18d4ac88bd6e64e0b783b351d
deanturpin <kali@brightcoin.uk>
2018-10-15 08:35:00 +0100

    Calculate ratios

--- arbitrage.pl
+++ arbitrage.pl
@@ -17,10 +17,10 @@ try:
     r = requests.get(url)
     all_coins = r.json()
 
-    # Construct a list of URLs
+    # Construct a list of URLs for the currency pair we're interested in
     trades = []
     sym1 = "BTC"
-    sym2 = "GBP"
+    sym2 = "USD"
     for exchange in all_coins:
       for from_symbol in all_coins[exchange]:
         if from_symbol in sym1:
@@ -28,7 +28,9 @@ try:
             if to_symbol in sym2:
               trades.append([exchange, from_symbol, to_symbol])
 
-    # Get prices
+    print(len(trades), "viable exchanges")
+
+    # Get latest rates
     prices = []
     for t in trades:
       exchange = t[0]
@@ -37,12 +39,20 @@ try:
       price = r.json()
       prices.append([exchange, float(price[sym2])])
 	  
-    # Sort and print
-    prices.sort()
+    # Calculate exchange ratios between exchanges
     i = 0
+    ri = len(prices) - 1
+    rates = []
     while i < len(prices):
-      print(prices[i][0], prices[i][1] / prices[len(prices) - 1][1])
+      rates.append([prices[i][1] / prices[ri][1], prices[i][0]
+                      + " > " + prices[ri][0]])
       i = i + 1
+      ri = ri - 1
+
+    # Sort and report
+    rates.sort()
+    for r in rates:
+      print(r[0], "\t", r[1])
 
 except Exception as e:
     print("exception ", e)
