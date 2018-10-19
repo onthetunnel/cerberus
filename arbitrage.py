d0adf93bf50fbb650c78212936d2744de7186b3d
deanturpin <kali@brightcoin.uk>
2018-10-19 08:36:48 +0100

    Add a list of currencies to arbitrage

--- arbitrage.py
+++ arbitrage.py
@@ -3,6 +3,7 @@
 import json
 import requests
 from collections import deque
+
 # Create a CryptoCompare URL from an exchange and a pair of symbols
 def construct_url(exchange, from_symbol, to_symbol):
   url = "https://min-api.cryptocompare.com/data/price?"
@@ -13,45 +14,54 @@ def construct_url(exchange, from_symbol, to_symbol):
 
 # Get the spot price for a trade
 def get_spot(exchange, from_symbol, to_symbol):
-      url = construct_url(exchange, from_symbol, to_symbol)
-      r = requests.get(url)
-      price = r.json()
-      return float(price[to_symbol])
+  url = construct_url(exchange, from_symbol, to_symbol)
+  r = requests.get(url)
+  price = r.json()
+  return float(price[to_symbol])
 
 def tabulate(table):
+  # Only print the top rows
+  index = 0
+  print()
+  for row in table:
+    for cell in row:
+      print(cell, end="|")
     print()
-    for row in table:
-      for cell in row:
-        print(cell, end="|")
-      print()
-    print("\n")
+    index += 1
+    if index >= 10:
+        break
+  print("\n")
 
 try:
 
-    # Get entry and exit currencies
-    f = open("currencies.txt")
-    currencies = deque(f.read().split())
-    entry_currency = currencies[0]
-    exit_currency = currencies[1]
+  # Get entry and exit currencies
+  f = open("currencies.txt")
+  currencies = deque(f.read().split())
 
-    # Get all pairs from all exchanges
-    url = 'https://min-api.cryptocompare.com/data/all/exchanges'
-    r = requests.get(url)
-    all_coins = r.json()
+  # Get all pairs from all exchanges
+  url = 'https://min-api.cryptocompare.com/data/all/exchanges'
+  r = requests.get(url)
+  all_coins = r.json()
 
-    # Exchanges we're not interested in
-    exchange_blacklist = ["MonetaGo", "Lykke", "CCEDK", "Zecoex", "ExtStock",
-            "EthexIndia", "Quoine", "Yacuna", "BTCE", "Cryptsy", "Abucoins",
-            "WEX", "Cexio", "CCEX", "Coinsetter", "Bitlish", "BTER", "LocalBitcoins",
-            "Yobit", "WavesDEX"]
+  # Exchanges we're not interested in
+  exchange_blacklist = ["MonetaGo", "Lykke", "CCEDK", "Zecoex", "ExtStock",
+          "EthexIndia", "Quoine", "Yacuna", "BTCE", "Cryptsy", "Abucoins",
+          "WEX", "Cexio", "CCEX", "Coinsetter", "Bitlish", "BTER", "LocalBitcoins",
+          "Yobit", "WavesDEX"]
 
-    # Get all currency pairs for all exchanges
-    currency_pairs = []
-    for exchange in all_coins:
-      if exchange not in exchange_blacklist:
-        for from_symbol in all_coins[exchange]:
-          for to_symbol in all_coins[exchange][from_symbol]:
-            currency_pairs.append([exchange, from_symbol, to_symbol])
+  # Get all currency pairs for all exchanges
+  currency_pairs = []
+  for exchange in all_coins:
+    if exchange not in exchange_blacklist:
+      for from_symbol in all_coins[exchange]:
+        for to_symbol in all_coins[exchange][from_symbol]:
+          currency_pairs.append([exchange, from_symbol, to_symbol])
+
+  # Calculate arbitrage opportunities each target currency
+  for c in currencies:
+
+    entry_currency = c
+    exit_currency = c
 
     # Extract the viable entry and exit points for our trade
     entry_points = []
@@ -91,6 +101,7 @@ try:
           arbitrage.append([spot2 / spot1, to1, exchange1, from1, exchange2, to2])
 
     # Arbitrage summary
+    print("#", c)
     print("*", len(currency_pairs), "currency pairs listed across all exchanges")
     print("* Calculating exchange rates from", entry_currency, "to", exit_currency,
             "via 1 crypto currency")
@@ -99,20 +110,9 @@ try:
     print("* Prices fetched using the [CryptoCompare API](https://min-api.cryptocompare.com/)\n")
 
     # Sort and report
-    print("# Triangular arbitrage")
-    print("Fiat to crypto to fiat.")
-
     arbitrage.sort()
     arbitrage.reverse()
     tabulate(arbitrage)
-
-    print("# Entry points")
-    print("Converting fiat to crypto currency.")
-    tabulate(entry_points)
-
-    print("# Exit points")
-    print("Converting crypto back to target fiat currency.")
-    tabulate(exit_points)
 
 except Exception as e:
     print("exception ", e)
